85029aab6c9f468aea4b3fb90a7f5ddf9a6cca45
Donn Zhao <z.donn@emondo.com.au>
2018-05-16 20:56:56 +1000

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+  return <header className="app-header">
+           <h2>NAVIGATION</h2>
+         </header>
+}
+
+export default header
