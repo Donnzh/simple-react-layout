682dc10a18340e0df2935e2c7bbef0336509856b
Donn Zhao <z.donn@emondo.com.au>
2018-05-16 20:58:24 +1000

    content component

---
+++ src/components/Content/index.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react';
+import './Content.css';
+
+export default class Content extends PureComponent {
+  renderContentBlocks = (blocks, activeBlock) => {
+    return blocks.map((b, i) => {
+      return (
+          <div
+              onClick={() => this.props.toggleBlockClick(b.name)}
+              key={i}
+              className="content-block"
+              style={{
+                height: activeBlock ?
+                    //calculate the average height in percentage
+                    (b.name === activeBlock ? '100%' : '0%') : `${100 / blocks.length}%`,
+                backgroundColor: b.color
+              }}>
+            <h3>{b.name}</h3>
+            <h5 className={`content-block-subTitle ${(b.name === activeBlock ? "subTitle-actived" : "")}`}
+            >IS ACTIVE!</h5>
+          </div>
+      )
+    })
+  }
+
+  render () {
+    const {
+      isHiddenSideBar,
+      blocks,
+      activeBlock,
+      toggleSideBar,
+    } = this.props
+    const contentClass = `app-content ${isHiddenSideBar ? '' : 'app-content-actived'}`;
+    return (
+        <div className={contentClass}>
+          <button
+              className="toggle-siderbar-button"
+              onClick={toggleSideBar}>
+            <span> {isHiddenSideBar ? '<' : '>' }</span>
+          </button>
+          {this.renderContentBlocks(blocks, activeBlock)}
+        </div>
+    );
+  }
+}
